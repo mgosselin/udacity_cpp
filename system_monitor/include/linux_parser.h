419a72ed92352f5e94ea00fc6836fd6e02478e3c
Michael Gosselin <mike.gosselin88@gmail.com>
2021-04-03 23:09:32 -0700

    commit header file for linux_parser namespace

--- system_monitor/include/linux_parser.h
+++ system_monitor/include/linux_parser.h
@@ -30,20 +30,21 @@ std::string Kernel();
 // CPU
 enum CPUStates {
   kUser_ = 0,
-  kNice_,
-  kSystem_,
-  kIdle_,
-  kIOwait_,
-  kIRQ_,
-  kSoftIRQ_,
-  kSteal_,
-  kGuest_,
-  kGuestNice_
+  kNice_ = 1,
+  kSystem_ = 2,
+  kIdle_ = 3,
+  kIOwait_ = 4,
+  kIRQ_ = 5,
+  kSoftIRQ_ = 6,
+  kSteal_ = 7,
+  kGuest_ = 8,
+  kGuestNice_ = 9
 };
 std::vector<std::string> CpuUtilization();
 long Jiffies();
 long ActiveJiffies();
 long ActiveJiffies(int pid);
+long StartJiffies(int pid);
 long IdleJiffies();
 
 // Processes
@@ -51,7 +52,7 @@ std::string Command(int pid);
 std::string Ram(int pid);
 std::string Uid(int pid);
 std::string User(int pid);
-long int UpTime(int pid);
+long UpTime(int pid);
 };  // namespace LinuxParser
 
 #endif

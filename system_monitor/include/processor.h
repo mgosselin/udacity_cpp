dbe5019c958ecddd645ff4fe1911702b7ce1b4ea
Michael Gosselin <mike.gosselin88@gmail.com>
2021-04-03 23:14:48 -0700

    define the utilization function in the processor class

--- system_monitor/include/processor.h
+++ system_monitor/include/processor.h
@@ -1,12 +1,16 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include "linux_parser.h"
+
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  float Utilization();  // DONE: See src/processor.cpp
 
-  // TODO: Declare any necessary private members
+  // DONE: Declare any necessary private members
  private:
+  long prev_idle_;
+  long prev_total_;
 };
 
 #endif
--- system_monitor/src/processor.cpp
+++ system_monitor/src/processor.cpp
@@ -1,4 +1,14 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// DONE: Return the aggregate CPU utilization
+float Processor::Utilization() {
+  // used this stack overflow post:
+  // https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux/23376195#23376195
+  long curr_idle = LinuxParser::IdleJiffies();
+  long curr_total = LinuxParser::Jiffies();
+  long diff_idle = curr_idle - prev_idle_;
+  long diff_total = curr_total - prev_total_;
+  prev_idle_ = curr_idle;
+  prev_total_ = curr_total;
+  return (float)(diff_total - diff_idle) / (float)diff_total;
+}

8ab48dc299d2f3d475d108ab8819b5038e462031
Michael Gosselin <mike.gosselin88@gmail.com>
2021-04-03 22:55:10 -0700

    define functions and constructor in process source file

--- system_monitor/src/process.cpp
+++ system_monitor/src/process.cpp
@@ -10,23 +10,36 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Constructor
+Process::Process(int pid) {
+  pid_ = pid;
+}
+
+// DONE: Return this process's ID
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { return 0; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// DONE: Return the command that generated this process
+string Process::Command() {
+  return LinuxParser::Command(pid_);
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// DONE: Return this process's memory utilization
+string Process::Ram() {
+  return LinuxParser::Ram(pid_);
+}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// DONE: Return the user (name) that generated this process
+string Process::User() {
+  return LinuxParser::User(pid_);
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// DONE: Return the age of this process (in seconds)
+long Process::UpTime() {
+  return LinuxParser::UpTime(pid_);
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
